2dc3581a831f07e1570e2a6be5e9ed122192cb96
chiharakenta <chiharakenta0519@gmail.com>
2020-02-10 14:02:01 +0900

    add build directory

---
+++ build/precache-manifest.0d4894e8803f4f8c27fbe51fddfd3e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2190ccd4e72cbfbd5101223c675a1ef4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44ecd78873efc53456cc",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "396dad2242b6851b1cee",
+    "url": "/static/js/2.1253ad35.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.1253ad35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44ecd78873efc53456cc",
+    "url": "/static/js/main.6c559d8f.chunk.js"
+  },
+  {
+    "revision": "6aac407fb49db83d1512",
+    "url": "/static/js/runtime-main.c3561e1b.js"
+  }
+]);
